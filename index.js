cf97d42a53eeb61c4d2d51e85259c580c7942a89
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-09 20:59:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,80 +1,50 @@
-let secs = 0;
-let mins = 0;
-let hrs = 0;
-let dispSecs = 0;
-let dispMins = 0;
-let dispHrs = 0;
-let interval = null;
-let state = "stopped"
-function stopWatch(){
-    secs++;
-    if(secs/60==1){
-        secs = 0;
-        mins++;
-        if(mins/60==1){
-            mins=0;
-            hrs++;
+const date = new Date();
+const renderCalendar = function() {
+    const monthDays = document.querySelector(".days");
+    const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = date.getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+    const nextDays = 7- lastDayIndex;
+    const months = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+    document.querySelector(".date p").innerHTML = date.toDateString();
+    let days = "";    
+    for(let x = firstDayIndex; x>0; x--){
+        days = days+ "<div class = 'prev-date'>" + (prevLastDay-x+1) + "</div>";
+    }
+    for(let i=1; i<=lastDay; i++){
+        if(i===new Date().getDate() && date.getMonth()===new Date().getMonth()){
+        days = days + "<div class = 'today'>" + i + "</div>";
+        }else{
+            days = days + "<div>" + i + "</div>";
         }
+        monthDays.innerHTML = days;
     }
-    if(secs<10){
-        dispSecs = "0"+secs.toString();
+    for(let j=1; j<=nextDays; j++){
+        days = days +"<div class = 'next-date'>" +j + "</div>";
+        monthDays.innerHTML = days;
     }
-    else{
-        dispSecs = secs;
-    }
-    if(mins<10){
-        dispMins = "0"+mins.toString();
-    }
-    else{
-        dispMins = mins;
-    }
-    if(hrs<10){
-        dispHrs = "0"+hrs.toString();
-    }
-    else{
-        dispHrs = hrs;
-    }
-    document.getElementById("display").innerHTML = dispHrs+":"+dispMins+":"+dispSecs;
-}
-document.getElementById("pause").disabled =true;
-document.getElementById("stop").disabled =true;    
-function start(){
-   if(state == "stopped"){
-      interval = window.setInterval(stopWatch, 1000);
-      document.getElementById("pause").innerHTML = "pause";
-      state = "started";
-      document.getElementById("start").disabled = true; 
-      document.getElementById("pause").disabled = false;
-      document.getElementById("stop").disabled = false;      
-   }
-}
-function stop(){
-    if(state == "started"){
-        window.clearInterval(interval);
-        secs = 0;
-        mins = 0;
-        hrs = 0;
-        document.getElementById("display").innerHTML = "00:00:00";
-        document.getElementById("start").innerHTML = "start";
-        state = "stopped";
-        document.getElementById("start").disabled = false;
-        document.getElementById("pause").disabled = true;
-        document.getElementById("stop").disabled = true;
-    }
-}
-function pause(){
-    if(state == "started"){
-        window.clearInterval(interval);
-        document.getElementById("pause").innerHTML = "continue";
-        state = "stopped";
-        document.getElementById("start").disabled = true;
-        document.getElementById("stop").disabled = true;
-    }
-    else{
-        interval = window.setInterval(stopWatch, 1000);
-        document.getElementById("pause").innerHTML = "pause";
-        state = "started";
-        document.getElementById("stop").disabled = false;
-
-    }
-}
+} 
+document.querySelector(".prev").addEventListener("click", function(){
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+});
+document.querySelector(".next").addEventListener("click", function(){
+    date.setMonth(date.getMonth()+1);
+    renderCalendar();
+});
+renderCalendar();
